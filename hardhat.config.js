fe0748fcb6b8cce78c91502517b0e6e68df93f53
vagish@fibr.ai <vagish@fibr.ai>
2024-09-15 15:07:58 +0530

    Init

---
+++ hardhat.config.js
@@ -0,0 +1,47 @@
+// stuff imported here become tasks , they can be used as yarn hardhat <task name>
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomicfoundation/hardhat-verify"); // verify task
+require("./tasks/block-number"); // block-number task
+require("hardhat-gas-reporter");
+require("solidity-coverage"); // coverage task
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+
+module.exports = {
+  defaultNetwork: "hardhat", // chainId: 31337
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    localhost: {
+      // different fromt the default hardhat network
+      url: "http://127.0.0.1:8545/",
+      // accounts: hardhat provides these,
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+    ],
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    // outputFile: "gas-report.txt", -> enable gives in a file disable gives in terminal
+    noColors: false,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    // token: "MATIC", "BNB", "ETH" -> default
+  },
+};
